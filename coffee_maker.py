474db016bd7a43339e46614a6cbb1a9a1e330c85
Hetul Mistry <manishcmdgm812@gmail.com>
2025-08-29 19:27:12 +0530

    Add: Coffee maker class

---
+++ coffee_maker.py
@@ -0,0 +1,28 @@
+from CoffeeMaker.menu import MenuItem
+
+
+class CoffeeMaker:
+    def __init__(self) -> None:
+        self.resources: dict[str, int] = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+
+    def report(self) -> None:
+        print(f"Water: {self.resources['water']}ml")
+        print(f"Milk: {self.resources['milk']}ml")
+        print(f"Coffee: {self.resources['coffee']}g")
+
+    def is_resource_sufficient(self, drink: MenuItem) -> bool:
+        can_make: bool = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > self.resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                can_make = False
+        return can_make
+
+    def make_coffee(self, order: MenuItem) -> None:
+        for item in order.ingredients:
+            self.resources[item] -= order.ingredients[item]
+        print(f"Here is your {order.name} ☕️. Enjoy!")
